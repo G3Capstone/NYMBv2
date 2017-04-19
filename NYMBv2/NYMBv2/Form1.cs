e40a4e3dde27ca80a90cf9dd4dbf4297cd1dc82a
Erik Alegria <alegriae7021@student.faytechcc.edu>
2017-04-18 23:10:41 -0400

    Proposed design for the second interation of the program. basic and still needs some refining, you have to run it to see it working and drawing everything properly.

---
+++ NYMBv2/NYMBv2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NYMBv2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tabControl1.DrawItem += new DrawItemEventHandler(tabControl1_DrawItem);
+        }
+
+        private void tabControl1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Brush _textBrush;
+
+            // Get the item from the collection.
+            TabPage _tabPage = tabControl1.TabPages[e.Index];
+
+
+            // Get the real bounds for the tab rectangle.
+            Rectangle _tabBounds = tabControl1.GetTabRect(e.Index);
+
+
+            if (e.State == DrawItemState.Selected)
+            {
+                // Draw a different background color, and don't paint a focus rectangle.
+                _textBrush = new SolidBrush(Color.Black);
+                g.FillRectangle(Brushes.LightBlue, e.Bounds);
+            }
+            else
+            {
+                _textBrush = new System.Drawing.SolidBrush(e.ForeColor);
+                e.DrawBackground();
+            }
+
+            // gets the font property of the tabcontrol
+            Font _tabFont = tabControl1.Font;
+            
+
+            // Draw string. Center the text.
+            StringFormat _stringFlags = new StringFormat();
+            _stringFlags.Alignment = StringAlignment.Center;
+            _stringFlags.LineAlignment = StringAlignment.Center;
+            g.DrawString(_tabPage.Text, _tabFont, _textBrush, _tabBounds, new StringFormat(_stringFlags));
+            
+        }
+    }
+}
