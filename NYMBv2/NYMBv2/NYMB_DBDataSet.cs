b0925fcb4ea504b89f134d6315ee7549525a641c
alegriae7021 <alegriae7021@ATC104B07>
2017-04-20 18:31:18 -0400

    added the database stuffs

---
+++ NYMBv2/NYMBv2/NYMB_DBDataSet.cs
@@ -0,0 +1,15 @@
+﻿namespace NYMBv2
+{
+
+
+    partial class NYMB_DBDataSet
+    {
+    }
+}
+
+namespace NYMBv2.NYMB_DBDataSetTableAdapters {
+    
+    
+    public partial class BindersTableAdapter {
+    }
+}
