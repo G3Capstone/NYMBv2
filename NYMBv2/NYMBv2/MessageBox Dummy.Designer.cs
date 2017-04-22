834770cd8f5678cdf8da700d086f99c5848752da
James Alves <james.barclay.alves@gmail.com>
2017-04-21 23:54:53 -0400

    Revert "To be removed"
    This reverts commit 316b7f5e087704dc3926b8bda2dfb019425583c5.

--- NYMBv2/NYMBv2/MessageBox Dummy.Designer.cs
+++ NYMBv2/NYMBv2/MessageBox Dummy.Designer.cs
@@ -29,38 +29,10 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
-			this.nYMB_DataSet = new NYMBv2.NYMB_DataSet();
-			this.nYMBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
-			((System.ComponentModel.ISupportInitialize)(this.nYMB_DataSet)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.nYMBDataSetBindingSource)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// nYMB_DataSet
-			// 
-			this.nYMB_DataSet.DataSetName = "NYMB_DataSet";
-			this.nYMB_DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-			// 
-			// nYMBDataSetBindingSource
-			// 
-			this.nYMBDataSetBindingSource.DataSource = this.nYMB_DataSet;
-			this.nYMBDataSetBindingSource.Position = 0;
-			// 
-			// MessageBox_Dummy
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 265);
-			this.Name = "MessageBox_Dummy";
 			this.Text = "MessageBox_Dummy";
-			((System.ComponentModel.ISupportInitialize)(this.nYMB_DataSet)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.nYMBDataSetBindingSource)).EndInit();
-			this.ResumeLayout(false);
-
 		}
 
 		#endregion
-
-		private NYMB_DataSet nYMB_DataSet;
-		private System.Windows.Forms.BindingSource nYMBDataSetBindingSource;
 	}
 }
