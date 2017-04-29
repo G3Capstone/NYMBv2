8837e7030fc886cec9bbbd7a12ba2f24daf02d41
Raven Moore <ravenelainem@gmail.com>
2017-04-29 12:47:31 -0400

    Finished the password validation and the username validation for the most part. Having trouble getting the validation for usernames to check for certain symbols, such as (').

--- NYMBv2/NYMBv2/CreateUser.cs
+++ NYMBv2/NYMBv2/CreateUser.cs
@@ -34,10 +34,118 @@ namespace NYMBv2
 
         }
 
-        #region Validation Methods
+        #region Username Validation Methods
+
+        #region Username Length of 3-16 Characters
+
+        /**
+         * The CheckLength method takes a string arugement and
+         * checks if the username is within the character range.
+         */
+        private bool CheckUsernameLength(string password)
+        {
+            const int MIN_LENGTH = 3;       //minimum number of characters allowed in the username
+            const int MAX_LENGTH = 16;      //maximum number of characters allowed in the username
+            bool valid = false;             //Flags verification of category
+
+            if (password.Length >= MIN_LENGTH && password.Length <= MAX_LENGTH)
+            {
+                valid = true;
+            }
+            else
+            {
+                //flag that the password does not meet the necessary requirements.
+                valid = false;
+
+                //Show the error message below the password textbox.
+                lblUsernameErrMsg.Visible = true;
+
+                //Change the text to inform the user of the password error.
+                lblUsernameErrMsg.Text = "Username must be 3-16 characters long.";
+            }
+
+            return valid;
+
+        }
+
+        #endregion
+
+        #region Username shouldn't contain certain special characters
+
+        /*
+         * The CheckUsernameSpecial method takes a string arguement
+         * and checks if there are any certain special characters, 
+         * that shouldn't be in a username,such as (') and (&).
+         */
+
+        private bool CheckUsernameSpecial (string password)
+        {
+            int special = 0;        //Counts the number of unacceptable characters
+            bool valid;             //Flags the validity of the password.
+
+            //A username can't contain ('), ("), (&), (;), or (,).
+            //Count each char for every instance of the special characters.
+            foreach (char ch in password)
+            {
+                if (ch == '"')
+                {
+                    special++;
+                    
+                    if (ch == '&')
+                    {
+                        special++;
+
+                        if (ch == ';')
+                        {
+                            special++;
+
+                            if (ch == ',')
+                            {
+                                special++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            //Check if there are any unacceptable special characters in the username.
+            //If there are no forbidden characters in the username, return value is 
+            //true.
+            if (special == 0)
+            {
+                valid = true;
+            }
+            else
+            {
+                //flag that the password does not meet the necessary requirements.
+                valid = false;
+
+                //Show the error message below the password textbox.
+                lblUsernameErrMsg.Visible = true;
+
+                //Change the text to inform the user of the password error.
+                lblPasswordErrMsg.Text = "Username can't contain that character";
+
+            }
+
+            return valid;
+        }
+
+        #endregion
+
+
+        #endregion
+
+        #region Password Validation Methods
+
 
         #region Password Length of 8-12 Characters
-        private bool CheckLength (string password)
+
+        /**
+        * The CheckPasswordLength method takes a string arugement and
+        * checks if the password is within the character range.
+        */
+        private bool CheckPasswordLength (string password)
         {
             const int MIN_LENGTH = 8;       //minimum number of characters allowed in the password
             const int MAX_LENGTH = 12;      //maximum number of characters allowed in the password
@@ -68,10 +176,11 @@ namespace NYMBv2
         #region Check for Uppercase Letters
 
         /**
-         * The NumberUppercase method accepts a string argument
-         * returns the number of uppercase letters it contains
-         */
-        private bool NumberUpperCase(string password)
+        * The CheckPasswordUppercase method accepts a string argument
+        * returns true if the password entered contains at least
+        * 1 uppercase letter
+        */
+        private bool CheckPasswordUpperCase(string password)
         {
             int uppercase = 0;      //The number of uppercase letters in the password.
             bool valid = false;     //Flags verification of category 
@@ -104,11 +213,139 @@ namespace NYMBv2
                 }
 
             return valid;
-        }   
+        }
         #endregion
 
+        #region Check for Lowercase Letters
 
-            #region First Validation
+        /**
+         * The CheckPasswordLowercase method accepts a string argument
+         * returns true if the password entered contains at least
+         * 1 lowercase letter
+         */
+        private bool CheckLowerCase(string password)
+        {
+            int lowercase = 0;      //The number of lowercase letters in the password.
+            bool valid = false;     //Flags verification of category 
+
+            //Count the lowercase characters in the password.
+            foreach (char ch in password)
+            {
+                if (char.IsLower(ch))
+                {
+                    lowercase++;
+                }
+            }
+
+            //Check if there is at least 1 uppercase letter in the password.
+            if (lowercase >= 1)
+            {
+                valid = true;
+            }
+            else
+            {
+                //flag that the password does not meet the necessary requirements.
+                valid = false;
+
+                //Show the error message below the password textbox.
+                lblPasswordErrMsg.Visible = true;
+
+                //Change the text to inform the user of the password error.
+                lblPasswordErrMsg.Text = "Password must have at least 1 lowercase letter.";
+
+            }
+
+            return valid;
+        }
+        #endregion
+
+        #region Check for Digits
+
+        /**
+         * The CheckPasswordDigit method accepts a string argument
+         * returns true if the password entered contains at least
+         * 1 number
+         */
+        private bool CheckDigit(string password)
+        {
+            int digit = 0;          //The number of digits in the password.
+            bool valid = false;     //Flags verification of category 
+
+            //Count the digits characters in the password.
+            foreach (char ch in password)
+            {
+                if (char.IsDigit(ch))
+                {
+                    digit++;
+                }
+            }
+
+            //Check if there is at least 1 digit in the password.
+            if (digit >= 1)
+            {
+                valid = true;
+            }
+            else
+            {
+                //flag that the password does not meet the necessary requirements.
+                valid = false;
+
+                //Show the error message below the password textbox.
+                lblPasswordErrMsg.Visible = true;
+
+                //Change the text to inform the user of the password error.
+                lblPasswordErrMsg.Text = "Password must have at least 1 digit.";
+
+            }
+
+            return valid;
+        }
+        #endregion
+
+        #region Check for Special
+
+        /**
+        * The CheckPasswordSpecial method accepts a string argument
+        * returns true if the password entered contains at least
+        * 1 special character
+        */
+        private bool CheckSpecial(string password)
+        {
+            int special = 0;        //The number of special characters in the password.
+            bool valid = false;     //Flags verification of category 
+
+            //Count the special characters in the password.
+            foreach (char ch in password)
+            {
+                if (char.IsPunctuation(ch))
+                {
+                    special++;
+                }
+            }
+
+            //Check if there is at least 1 digit in the password.
+            if (special >= 1)
+            {
+                valid = true;
+            }
+            else
+            {
+                //flag that the password does not meet the necessary requirements.
+                valid = false;
+
+                //Show the error message below the password textbox.
+                lblPasswordErrMsg.Visible = true;
+
+                //Change the text to inform the user of the password error.
+                lblPasswordErrMsg.Text = "Password must have at least 1 special character.";
+
+            }
+
+            return valid;
+        }
+        #endregion
+
+        #region First Validation
         private void ValidatePassword(string password)
         {
             const int MIN_LENGTH = 8;     //The lowest number of characters a password can contain.
@@ -131,6 +368,7 @@ namespace NYMBv2
 
 
         #endregion
+
 
 
     }
