0fb190630f53a22f2691c74e18a6b3b52619382d
Erik Alegria <alegriae7021@student.faytechcc.edu>
2017-04-30 17:08:18 -0400

    random commit so i can pull

---
+++ NYMBv2/NYMBv2/Events Dummy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NYMBv2
+{
+    public partial class Events_Dummy : Form
+    {
+
+         thing = 0;
+
+        public Events_Dummy()
+        {
+            InitializeComponent();
+        }
+    }
+}
