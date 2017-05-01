d9a8940d145f844eab3ad59cad6aa54e9bd3ef28
Raven Moore <ravenelainem@gmail.com>
2017-04-30 22:00:53 -0400

    More validation statements for the password for create user tab

--- NYMBv2/NYMBv2/Events Dummy.cs
+++ NYMBv2/NYMBv2/Events Dummy.cs
@@ -7,17 +7,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace NYMBv2
 {
     public partial class Events_Dummy : Form
     {
 
-         thing = 0;
+        #region Connection String
+        public string connectionString = ConfigurationManager.ConnectionStrings["NYMBv2.Properties.Settings.NYMBv2_DBConnectionString"].ConnectionString;
+        #endregion
+
 
         public Events_Dummy()
         {
             InitializeComponent();
         }
+
+        private void monthCalendarEvents_DateSelected(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        #region Display Announcements
+
+        private void DisplayAnnouncements()
+        {
+            var Announcementlist = new List<Announcement>();
+
+
+            //foreach statement that will repeat for the number of announcments in the database.
+            //Announcment entry = new announcment();	//the () will contain headline, content, start and end date for the announcment.
+            //list.Add(entry);
+            //if(entry._start <= currentdate)
+            //add entry to panelAnnouncmentStream
+
+            GetAnnouncements(Announcementlist);
+
+            rtbEvents.Clear();
+            rtbEvents.SelectionAlignment = HorizontalAlignment.Center;
+            rtbEvents.SelectedText += "\n <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> \n";
+
+            foreach (Announcement bulletin in Announcementlist)
+            {
+                rtbEvents.SelectedText += "\n~~~~~  " + bulletin._headline.ToString() + "  ~~~~~";
+                rtbEvents.SelectedText += "\n";
+                rtbEvents.SelectedText += "\n " + bulletin._content.ToString() + " \n";
+                rtbEvents.SelectedText += "\n <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> \n";
+            }
+
+        }
+
+
+        #endregion
+
+        #region Get Announcements
+        private List<List<Event>> GetEvents(List<List<Event>> EventList)
+        {
+            //Create a connection to the database
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                //SQL select statement
+                string query = @"SELECT [Subject], [Body] FROM [dbo].[Announcements] WHERE ([StartDisplayDate] <= GETDATE()) AND (GETDATE() < [EndDisplayDate])";
+
+                //Create a SQLCommand, passing the query and the connection
+                SqlCommand cmd = new SqlCommand(query, connection);
+
+                //connecting the SQLCommand Control and the database
+                cmd.Connection.Open();
+
+                //Creating a SQLDataReader to read the results of the query ran by the
+                // SQLCommand ExecuteReader function
+                using (SqlDataReader sql_reader = cmd.ExecuteReader())
+                {
+
+
+                    while (sql_reader.Read())
+                    {
+                        EventList.Add(new Event(sql_reader["Subject"].ToString(), sql_reader["body"].ToString()));
+                    }
+                }
+            }
+
+            return EventList;
+        }
+        #endregion
     }
 }
--- NYMBv2/NYMBv2/Inventory Dummy.Designer.cs
+++ NYMBv2/NYMBv2/Inventory Dummy.Designer.cs
@@ -28,19 +28,62 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.SuspendLayout();
-			// 
-			// Inventory_Dummy
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(688, 606);
-			this.Name = "Inventory_Dummy";
-			this.Text = "Inventory_Dummy";
-			this.ResumeLayout(false);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblEvents = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.splitContainer1);
+            this.groupBox1.Controls.Add(this.lblEvents);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(917, 669);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // lblEvents
+            // 
+            this.lblEvents.Font = new System.Drawing.Font("Arial", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEvents.Location = new System.Drawing.Point(1, 13);
+            this.lblEvents.Name = "lblEvents";
+            this.lblEvents.Size = new System.Drawing.Size(906, 35);
+            this.lblEvents.TabIndex = 0;
+            this.lblEvents.Text = "Events";
+            this.lblEvents.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(0, 51);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Size = new System.Drawing.Size(916, 617);
+            this.splitContainer1.SplitterDistance = 224;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // Inventory_Dummy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 696);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Inventory_Dummy";
+            this.Text = "Inventory_Dummy";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
 
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblEvents;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
 }
