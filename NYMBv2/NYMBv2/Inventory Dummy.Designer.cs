df1227f43d5b3373c2227504593d00d00de4c536
James Alves <james.barclay.alves@gmail.com>
2017-05-01 03:00:29 -0400

    More work on inventory logic

--- NYMBv2/NYMBv2/Inventory Dummy.Designer.cs
+++ NYMBv2/NYMBv2/Inventory Dummy.Designer.cs
@@ -28,24 +28,13 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(57, 59);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
 			// 
 			// Inventory_Dummy
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(688, 606);
-			this.Controls.Add(this.button1);
 			this.Name = "Inventory_Dummy";
 			this.Text = "Inventory_Dummy";
 			this.ResumeLayout(false);
@@ -53,7 +42,5 @@
 		}
 
 		#endregion
-
-		private System.Windows.Forms.Button button1;
 	}
 }
