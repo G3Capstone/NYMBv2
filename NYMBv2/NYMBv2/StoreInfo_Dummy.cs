935cbad74d11805786a93435dcf84be0343a8ccd
Raven Moore <ravenelainem@gmail.com>
2017-05-05 01:17:22 -0400

    Working on StoreInfo algorithm

--- NYMBv2/NYMBv2/StoreInfo_Dummy.cs
+++ NYMBv2/NYMBv2/StoreInfo_Dummy.cs
@@ -21,5 +21,36 @@ namespace NYMBv2
         {
 
         }
+
+
+        #region read from the database table.
+
+        /*
+        var list = new List<StoreInfo>();
+        object info = d.Rows[0].ItemArray[8];
+       
+
+
+            //foreach statement that will repeat for the number of announcments in the database.
+            //Announcment entry = new announcment();	//the () will contain headline, content, start and end date for the announcment.
+            //list.Add(entry);
+            //if(entry._start <= currentdate)
+            //add entry to panelAnnouncmentStream
+
+            GetStoreInfo(list);
+
+            locationLbl.Clear();
+            locationLbl.SelectionAlignment = HorizontalAlignment.Center;
+            locationLbl.SelectedText += "\n <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> \n";
+
+            foreach (StoreInfo info in list)
+            {
+                if()
+                locationLbl.SelectedText +=  "\n~~~~~  "  + bulletin._headline.ToString() + "  ~~~~~";
+                locationLbl.SelectedText += "\n";
+                locationLbl.SelectedText +=  "\n " + bulletin._content.ToString() + " \n";
+            }
+            */
+        #endregion
     }
 }
