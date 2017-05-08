e9bef24201bb074c5ccf6d0507eb1404111cba0e
Raven Moore <ravenelainem@gmail.com>
2017-05-08 00:43:20 -0400

    Made the StoreInfo constructor and worked on the StoreInfo code.

---
+++ NYMBv2/NYMBv2/StoreInfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYMBv2
+{
+    class StoreInfo
+    {
+        public string _storename { get; set; }
+
+        public string _owners { get; set; }
+
+        public string _ownerspic { get; set; }
+
+        public string _location { get; set; }
+
+        public string _storepic { get; set; }
+
+        public int _phone { get; set; }
+
+        public string _hours { get; set; }
+
+        public string _description { get; set; }
+
+
+        public StoreInfo()
+        {
+        }
+
+        public StoreInfo(string s, string o, string op, string l, string sp,
+                         int p, string h, string d)
+        {
+            _storename = s;
+            _owners = o;
+            _ownerspic = op;
+            _location = l;
+            _storepic = sp;
+            _phone = p;
+            _hours = h;
+            _description = d;
+            
+        }
+
+    }
+}
--- NYMBv2/NYMBv2/StoreInfo_Dummy.cs
+++ NYMBv2/NYMBv2/StoreInfo_Dummy.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace NYMBv2
 {
     public partial class StoreInfo_Dummy : Form
     {
+        #region Connection String
+        public string connectionString = ConfigurationManager.ConnectionStrings["NYMBv2.Properties.Settings.NYMBv2_DBConnectionString"].ConnectionString;
+        #endregion
+
         public StoreInfo_Dummy()
         {
             InitializeComponent();
@@ -25,32 +31,63 @@ namespace NYMBv2
 
         #region read from the database table.
 
-        /*
-        var list = new List<StoreInfo>();
-        object info = d.Rows[0].ItemArray[8];
-       
+
+        private void ReadStoreInfo()
+        {
+            string storename;       //Holds the string output of the storename cell.
+            string description;     //Holds the string output of the description cell.
+            string location;        //Holds the string output of the location cell.
 
 
-            //foreach statement that will repeat for the number of announcments in the database.
-            //Announcment entry = new announcment();	//the () will contain headline, content, start and end date for the announcment.
-            //list.Add(entry);
-            //if(entry._start <= currentdate)
-            //add entry to panelAnnouncmentStream
+            var list = new List<StoreInfo>();
+            object info = d.Rows[0].ItemArray[8];
 
-            GetStoreInfo(list);
 
-            locationLbl.Clear();
-            locationLbl.SelectionAlignment = HorizontalAlignment.Center;
-            locationLbl.SelectedText += "\n <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> \n";
+                GetStoreInfo(list);
 
-            foreach (StoreInfo info in list)
-            {
-                if()
-                locationLbl.SelectedText +=  "\n~~~~~  "  + bulletin._headline.ToString() + "  ~~~~~";
-                locationLbl.SelectedText += "\n";
-                locationLbl.SelectedText +=  "\n " + bulletin._content.ToString() + " \n";
-            }
-            */
+                locationLbl.Clear();
+                locationLbl.SelectionAlignment = HorizontalAlignment.Center;
+
+                foreach (StoreInfo info in list)
+                {
+                    
+                    locationLbl.Text += info._description.ToString();
+                    
+                    
+                }
+                
+        }
         #endregion
+
+        #region Get Store Info
+        private List<StoreInfo> GetAnnouncements(List<StoreInfo> StoreInfoList)
+        {
+            //Create a connection to the database
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                //SQL select statement
+                string query = @"SELECT [Description], [Location] FROM [dbo].[StoreInfo]";
+
+                //Create a SQLCommand, passing the query and the connection
+                SqlCommand cmd = new SqlCommand(query, connection);
+
+                //connecting the SQLCommand Control and the database
+                cmd.Connection.Open();
+
+                //Creating a SQLDataReader to read the results of the query ran by the
+                // SQLCommand ExecuteReader function
+                using (SqlDataReader sql_reader = cmd.ExecuteReader())
+                {
+                    while (sql_reader.Read())
+                    {
+                        StoreInfoList.Add(new StoreInfo(sql_reader["Description"].ToString(), sql_reader["Location"].ToString()));
+                    }
+                }
+            }
+
+            return StoreInfoList;
+        }
+        #endregion
+
     }
 }
